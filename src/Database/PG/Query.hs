9d8a014a4b21ce329d0978a97d5f64031dca86c6
rakeshkky <rakesh.emmadi21c@gmail.com>
2019-02-06 16:58:26 +0530

    add 'Listen' module to exports of Query.hs

--- src/Database/PG/Query.hs
+++ src/Database/PG/Query.hs
@@ -3,11 +3,13 @@ module Database.PG.Query
     , module Database.PG.Query.Pool
     , module Database.PG.Query.Class
     , module Database.PG.Query.Connection
+    , module Database.PG.Query.Listen
     ) where
 
 import           Database.PG.Query.Class
 import           Database.PG.Query.Connection  (ConnInfo (..), PGConn (..),
                                                 PGConnErr (..), PrepArg,
                                                 ResultOk (..), Template)
+import           Database.PG.Query.Listen
 import           Database.PG.Query.Pool
 import           Database.PG.Query.Transaction
