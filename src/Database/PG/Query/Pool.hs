2d613b6a3b8033a50c894ea38cabe7c03862e7d0
Vamshi Surabhi <vamshi@hasura.io>
2020-08-27 17:25:43 +0530

    Revert "add instrumentation to get connection pool stats (#23)"
    This reverts commit d9cd4caf4504bf3f8d1e9fff68e89fc14c54c67c.

--- src/Database/PG/Query/Pool.hs
+++ src/Database/PG/Query/Pool.hs
@@ -1,17 +1,15 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -fno-warn-missing-fields #-}
 
 module Database.PG.Query.Pool
   ( ConnParams (..)
   , PGPool
-  , PGPoolResource (..)
   , withExpiringPGconn
   , defaultConnParams
   , initPGPool
-  , initPGPoolResource
   , destroyPGPool
   , withConn
   , runTxOnConn'
@@ -31,15 +29,14 @@ module Database.PG.Query.Pool
   , PGConnectionStale(..)
   ) where
 
-import           Database.PG.ExtraBindings
 import           Database.PG.Query.Connection
 import           Database.PG.Query.Transaction
+import           Database.PG.ExtraBindings
 
 import           Control.Exception
 import           Control.Monad.Except
 import           Control.Monad.Trans.Control
 import           Data.Aeson
-import           Data.Int                      (Int64)
 import           Data.IORef
 import           Data.Time
 import           GHC.Exts                      (fromString)
@@ -52,19 +49,6 @@ import qualified Data.Text                     as T
 import qualified Database.PostgreSQL.LibPQ     as PQ
 
 type PGPool = RP.Pool PGConn
-
--- | A type with 'PGPool' and some metrics related to the pool. The
--- resource-pool package doesn't provide any primitives to get any details about
--- all the resources in the pool, or even a function that can fold or collect
--- over all resources in the pool (which makes sense because that might mean
--- acquiring all the resources in the pool at once). Hence we introduce a new
--- type with the 'PGPool' and functions which can return relevant metrics or
--- statistics regarding the pool.
-data PGPoolResource
-  = PGPoolResource
-  { pgResourcePool        :: !PGPool
-  , pgGetTotalConnections :: !(IO Int64)
-  }
 
 data ConnParams
   = ConnParams
@@ -81,46 +65,23 @@ data ConnParams
 defaultConnParams :: ConnParams
 defaultConnParams = ConnParams 1 20 60 True Nothing
 
-data ConnectionHooks
-  = ConnectionHooks
-  { connectionHookPostCreate  :: PGConn -> IO ()
-    -- ^ post connection init hook
-  , connectionHookPostDestroy :: PGConn -> IO ()
-    -- ^ post connection destroy hook
-  }
-
--- | This function will create a 'PGPool' and create relevant functions which
--- can compute metrics/stats related to the pool.
-initPGPoolResource
-  :: ConnInfo -> ConnParams -> PGLogger -> IO PGPoolResource
-initPGPoolResource ci cp logger = do
-  totalConnections <- newIORef 0
-  let increment = const $ atomicModifyIORef' totalConnections (\c -> (c + 1, ()))
-      decrement = const $ atomicModifyIORef' totalConnections (\c -> (max 0 (c - 1), ()))
-      hooks = ConnectionHooks increment decrement
-  pool <- initPGPool ci cp logger hooks
-  return $ PGPoolResource pool (readIORef totalConnections)
-
 initPGPool :: ConnInfo
            -> ConnParams
            -> PGLogger
-           -> ConnectionHooks
            -> IO PGPool
-initPGPool ci cp logger ConnectionHooks{..} = do
+initPGPool ci cp logger =
   RP.createPool creator destroyer nStripes diffTime nConns
   where
     nStripes  = cpStripes cp
     nConns    = cpConns cp
-    retryP    = mkPGRetryPolicy $ ciRetries ci
+    retryP = mkPGRetryPolicy $ ciRetries ci
     creator   = do
       createdAt <- getCurrentTime
       pqConn  <- initPQConn ci logger
       ctr     <- newIORef 0
       table   <- HI.new
-      let pgConn = PGConn pqConn (cpAllowPrepare cp) retryP logger ctr table createdAt (cpMbLifetime cp)
-      connectionHookPostCreate pgConn
-      return pgConn
-    destroyer conn = PQ.finish (pgPQConn conn) >> connectionHookPostDestroy conn
+      return $ PGConn pqConn (cpAllowPrepare cp) retryP logger ctr table createdAt (cpMbLifetime cp)
+    destroyer = PQ.finish . pgPQConn
     diffTime  = fromIntegral $ cpIdleTime cp
 
 -- |
@@ -253,7 +214,7 @@ sqlFromFile fp = do
 --
 -- Note that idle connections that aren't actively expired here will be
 -- destroyed per the timeout policy in Data.Pool.
-withExpiringPGconn
+withExpiringPGconn 
   :: (MonadBaseControl IO m, MonadIO m)=> PGPool -> (PGConn -> m a) -> m a
 withExpiringPGconn pool f = do
   -- If the connection was stale, we'll discard it and retry, possibly forcing
@@ -261,7 +222,7 @@ withExpiringPGconn pool f = do
   handleLifted (\PGConnectionStale -> withExpiringPGconn pool f) $ do
     RP.withResource pool $ \connRsrc@PGConn{..} -> do
       now <- liftIO $ getCurrentTime
-      let connectionStale =
+      let connectionStale = 
             maybe False (\lifetime-> now `diffUTCTime` pgCreatedAt > lifetime) pgMbLifetime
       when connectionStale $ do
         -- Throwing is the only way to signal to resource pool to discard the
