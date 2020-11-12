8609ef44bc5b3736a777ac35fa45a217b47a051a
Brandon Simmons <brandon@hasura.io>
2020-11-12 16:56:02 -0500

    Remove libpq buffer shrinking, effectively reverting fbb635023 (#26)
    We're unfortunately getting segfaults occasionally. The plan is to rely
    on discarding connections periodically for now.

--- src/Database/PG/Query/Pool.hs
+++ src/Database/PG/Query/Pool.hs
@@ -234,8 +234,9 @@ withExpiringPGconn pool f = do
         throw PGConnectionStale
       -- else proceed with callback:
       f connRsrc
+        -- FIXME this segfaults sometimes... see cbits/libpq-bindings.c
         -- Clean up the connection buffers to prevent memory bloat (See #5087):
-        <* liftIO (unsafeClampInOutBufferSpace pgPQConn)
+        -- <* liftIO (unsafeClampInOutBufferSpace pgPQConn)
 
 -- | Used internally (see 'withExpiringPGconn'), but exported in case we need
 -- to allow callback to signal that the connection should be destroyed and we
