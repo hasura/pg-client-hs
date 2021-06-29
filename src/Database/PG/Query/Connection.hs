a99cbe01b8e32ec09106293cf1395d0184d2133e
Robert Vollmert <rob@vllmrt.net>
2021-06-29 17:34:34 +0200

    document `interruptible`
    lots of waffling, and I fear this is in more authoritative style
    than justified :/

--- src/Database/PG/Query/Connection.hs
+++ src/Database/PG/Query/Connection.hs
@@ -385,18 +385,43 @@ cancelPG c = do
     Left err -> throwIO $ CEError $ lenientDecodeUtf8 err
     Right () -> pure ()
 
+-- | interruptible runs the given action in in a separate thread,
+-- running the given cancel action before passing on any asynchronous
+-- exceptions to that thread. The intent is that
+--   `interruptible (pure ()) == id`
+-- in all respects (including exception handling), assuming the wrapped
+-- action behaves somewhat reasonably (i.e., doesn't swallow asynchronous
+-- exceptions). Particularly, we guarantee that the separate thread terminates
+-- before we return.
+--
+-- The point of this is to allow breaking out of blocking actions if they
+-- provide some cancelling escape hatch.
 interruptible :: IO () -> IO a -> IO a
 interruptible cancel action = mask $ \restore -> do
   a <- async action
+
+  -- By using `try` with `waitCatch`, we can distinguish between asnychronous
+  -- exceptions received from the outside, and those thrown by the wrapped action.
+  -- (The latter shouldn't occur, but we also want to avoid throwing an exception
+  -- back at the thread below.)
   res <- tryAsync $ restore (waitCatch a)
   case res of
+    -- Due to the use of `waitCatch` above, the only exceptions that `tryAsync`
+    -- might catch are asynchronous exceptions received from the "outside".
+    -- Thus, the `Left` case is the only one where the async action has not
+    -- necessarily terminated.
     Left (e :: SomeAsyncException) -> do
+      -- Cancelling might throw an exception; we save that and re-raise it,
+      -- but not before doing or job of passing the asynchronous exception on
+      -- to our child and waiting for it to terminate.
       cancelRes <- try $ cancel
       throwTo (asyncThreadId a) e
       r <- wait a
       case cancelRes of
-        Left (e' :: SomeException) -> throwIO e'
-        _                          -> pure r
+        Left (cancelEx :: SomeException) -> throwIO cancelEx
+        _                                -> pure r
+    -- In the non-interrupted case, we "undo" the `try`, collapsing things
+    -- effectively to `restore (wait a)`.
     Right (Left e) ->
       throwIO e
     Right (Right r) ->
