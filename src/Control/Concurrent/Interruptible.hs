c9f25c090a16dbd42a04c4eae306c608ce1bff3d
Robert Vollmert <rob@vllmrt.net>
2021-06-30 19:38:00 +0200

    fix rethrowing of exceptions in the case of interrupt
    there was a bug here in that we could swallow an async exception
    that we handled and cancelled for:
    
    1. receive async exception in the control thread
    2. cancel the wrapped action
    3. wrapped action / child thread finishes
    4. throw async exception at child thread
    5. wait for child thread
    
    When 4 happens after 3, the exception is lost, and not raised as
    was intended by 5. Instead, we now throw any exceptions raised by
    5, and rethrow the original exception otherwise.

--- src/Control/Concurrent/Interruptible.hs
+++ src/Control/Concurrent/Interruptible.hs
@@ -5,6 +5,7 @@ module Control.Concurrent.Interruptible
     ) where
 
 import           Control.Concurrent.Async
+import qualified Control.Exception
 import           Control.Exception.Safe
 
 -- | interruptible runs the given action in in a separate thread,
@@ -36,12 +37,19 @@ interruptible interrupt action = mask $ \restore -> do
       -- Cancelling might throw an exception; we save that and re-raise it,
       -- but not before doing or job of passing the asynchronous exception on
       -- to our child and waiting for it to terminate.
-      interruptRes <- try $ interrupt
+      interruptRes :: Either SomeException () <- try $ interrupt
       throwTo (asyncThreadId a) e
-      r <- wait a
-      case interruptRes of
-        Left (cancelEx :: SomeException) -> throwIO cancelEx
-        _                                -> pure r
+      waitRes :: Either SomeException a <- tryAsync $ wait a
+      case (interruptRes, waitRes) of
+        (Left cancelEx, _)     -> throwIO cancelEx
+        -- waitEx could be an exception thrown by the action, or our async
+        -- exception bubbling back up
+        (Right _, Left waitEx) -> Control.Exception.throwIO waitEx
+        -- in case the async exits cleanly before receiving the exception, we
+        -- re-raise it manually so as to not swallow it, since the action
+        -- *was* interrupted
+        (Right _, Right _)     -> Control.Exception.throwIO e
+
     -- In the non-interrupted case, we "undo" the `try`, collapsing things
     -- effectively to `restore (wait a)`.
     Right (Left e) ->
