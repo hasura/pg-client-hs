fe404ea1299f165ea0d1e00f3628457f5c16cde7
Robert Vollmert <rob@vllmrt.net>
2021-09-27 18:20:49 +0200

    Reformat source code with ormolu

--- src/Database/PG/Query/Class.hs
+++ src/Database/PG/Query/Class.hs
@@ -2,72 +2,71 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Database.PG.Query.Class
-       ( WithCount(..)
-       , WithReturning(..)
-       , FromCol(..)
-       , fromColHelper
-       , FromRow(..)
-       , FromRes(..)
-       , ToPrepArg(..)
-       , toPrepValHelper
-       , ToPrepArgs(..)
-       , SingleRow(..)
-       , Discard(..)
-       , AltJ(..)
-       , JSON (..)
-       , JSONB (..)
-       ) where
+  ( WithCount (..),
+    WithReturning (..),
+    FromCol (..),
+    fromColHelper,
+    FromRow (..),
+    FromRes (..),
+    ToPrepArg (..),
+    toPrepValHelper,
+    ToPrepArgs (..),
+    SingleRow (..),
+    Discard (..),
+    AltJ (..),
+    JSON (..),
+    JSONB (..),
+  )
+where
 
-import           Control.Monad.Except
-import           Control.Monad.Identity
-import           Data.Int
-import           Data.Scientific                  (Scientific)
-import           Data.Time
-import           Data.Word
-import           GHC.Exts
+import Control.Monad.Except
+import Control.Monad.Identity
+import Data.Aeson qualified as J
+import Data.Attoparsec.ByteString.Char8 qualified as Atto
+import Data.ByteString qualified as B
+import Data.ByteString.Lazy qualified as BL
+import Data.Int
+import Data.Scientific (Scientific)
+import Data.Text qualified as T
+import Data.Text.Lazy qualified as TL
+import Data.Time
+import Data.UUID qualified as UUID
+import Data.Vector qualified as V
+import Data.Vector.Mutable qualified as VM
+import Data.Word
+import Database.PG.Query.Connection
+import Database.PG.Query.PTI qualified as PTI
+import Database.PostgreSQL.LibPQ qualified as PQ
+import GHC.Exts
+import PostgreSQL.Binary.Decoding qualified as PD
+import PostgreSQL.Binary.Encoding qualified as PE
 
-import qualified Data.Aeson                       as J
-import qualified Data.Attoparsec.ByteString.Char8 as Atto
-import qualified Data.ByteString                  as B
-import qualified Data.ByteString.Lazy             as BL
-import qualified Data.Text                        as T
-import qualified Data.Text.Lazy                   as TL
-import qualified Data.UUID                        as UUID
-import qualified Data.Vector                      as V
-import qualified Data.Vector.Mutable              as VM
-import qualified Database.PostgreSQL.LibPQ        as PQ
-import qualified PostgreSQL.Binary.Decoding       as PD
-import qualified PostgreSQL.Binary.Encoding       as PE
+data WithCount a = WithCount
+  { wcCount :: Word64,
+    wcValue :: a
+  }
+  deriving (Show, Eq)
 
-import           Database.PG.Query.Connection
-import qualified Database.PG.Query.PTI            as PTI
-
-data WithCount a
-  = WithCount
-  { wcCount :: Word64
-  , wcValue :: a
-  } deriving (Show, Eq)
-
-data WithReturning a
-  = WithReturning
-  { wrCount   :: Word64
-  , wrResults :: Maybe a
+data WithReturning a = WithReturning
+  { wrCount :: Word64,
+    wrResults :: Maybe a
   }
 
-newtype SingleRow a = SingleRow { getRow :: a } deriving (Show, Eq)
+newtype SingleRow a = SingleRow {getRow :: a} deriving (Show, Eq)
 
-newtype AltJ a = AltJ { getAltJ :: a }
+newtype AltJ a = AltJ {getAltJ :: a}
 
 mapEither :: (a -> c) -> (b -> d) -> Either a b -> Either c d
-mapEither f _ (Left a)  = Left $ f a
+mapEither f _ (Left a) = Left $ f a
 mapEither _ f (Right b) = Right $ f b
 
 instance (J.FromJSON a) => FromCol (AltJ a) where
   fromCol = fromColHelper (PD.fn $ mapEither fromString AltJ . J.eitherDecodeStrict)
 
 class FromCol a where
-  fromCol :: Maybe B.ByteString
-          -> Either T.Text a
+  fromCol ::
+    Maybe B.ByteString ->
+    Either T.Text a
 
 fromColHelper :: PD.Value a -> Maybe B.ByteString -> Either T.Text a
 fromColHelper f = maybe (throwError "encountered null") (PD.valueParser f)
@@ -125,7 +124,7 @@ instance FromCol UUID.UUID where
 
 instance FromCol a => FromCol (Maybe a) where
   fromCol Nothing = return Nothing
-  fromCol bs      = Just <$> fromCol bs
+  fromCol bs = Just <$> fromCol bs
 
 class FromRes a where
   fromRes :: ResultOk -> ExceptT T.Text IO a
@@ -136,22 +135,22 @@ instance FromRes ResultOk where
 instance FromRes () where
   fromRes (ResultOkEmpty _) =
     return ()
-  fromRes (ResultOkData _)  =
+  fromRes (ResultOkData _) =
     throwError "Expecting no data for (). Instead, status is 'TuplesOk'"
 
 instance FromRes Discard where
   fromRes (ResultOkEmpty _) =
     return $ Discard ()
-  fromRes (ResultOkData _)  =
+  fromRes (ResultOkData _) =
     return $ Discard ()
 
 newtype Discard = Discard ()
-                deriving (Show, Eq)
+  deriving (Show, Eq)
 
 parseWord64 :: B.ByteString -> Either T.Text Word64
 parseWord64 b = either buildE return parsed
   where
-    parsed   = Atto.parseOnly (Atto.decimal <* Atto.endOfInput) b
+    parsed = Atto.parseOnly (Atto.decimal <* Atto.endOfInput) b
     buildE e = Left $ "Couldn't parse Word64: " <> fromString e
 
 extractCount :: PQ.Result -> ExceptT T.Text IO Word64
@@ -179,6 +178,7 @@ instance FromRes a => FromRes (WithCount a) where
     return $ WithCount c a
 
 type ResultMatrix = V.Vector ResultRow
+
 type ResultRow = V.Vector (Maybe B.ByteString)
 
 {-# INLINE colInt #-}
@@ -192,13 +192,13 @@ rowInt (PQ.Row n) = fromIntegral n
 -- Should be used only after checking
 -- the status
 buildMat :: PQ.Result -> IO ResultMatrix
-buildMat r =  do
-  nr  <- PQ.ntuples r
-  nc  <- PQ.nfields r
+buildMat r = do
+  nr <- PQ.ntuples r
+  nc <- PQ.nfields r
   mvx <- VM.unsafeNew (rowInt nr)
-  forM_ [0..pred nr] $ \ir -> do
+  forM_ [0 .. pred nr] $ \ir -> do
     mvy <- VM.unsafeNew (colInt nc)
-    forM_ [0..pred nc] $ \ic ->
+    forM_ [0 .. pred nc] $ \ic ->
       VM.unsafeWrite mvy (colInt ic) =<< PQ.getvalue r ir ic
     vy <- V.unsafeFreeze mvy
     VM.unsafeWrite mvx (rowInt ir) vy
@@ -209,16 +209,20 @@ instance FromRow a => FromRes [a] where
     throwError "Expecting data. Instead, status is 'CommandOk'"
   fromRes (ResultOkData res) = do
     rm <- liftIO $ buildMat res
-    ExceptT $ return $ fmap V.toList $
-      sequence $ V.map fromRow rm
+    ExceptT $
+      return $
+        fmap V.toList $
+          sequence $ V.map fromRow rm
 
 instance FromRow a => FromRes (V.Vector a) where
   fromRes (ResultOkEmpty _) =
     throwError "Expecting data. Instead, status is 'CommandOk'"
   fromRes (ResultOkData res) = do
     rm <- liftIO $ buildMat res
-    ExceptT $ return $ sequence $
-      V.map fromRow rm
+    ExceptT $
+      return $
+        sequence $
+          V.map fromRow rm
 
 instance FromRow a => FromRes (SingleRow a) where
   fromRes (ResultOkEmpty _) =
@@ -241,12 +245,13 @@ instance FromRow a => FromRes (Maybe a) where
 
 colMismatch :: Int -> Int -> T.Text
 colMismatch expected actual =
-  fromString $ mconcat
-  [ "Expected "
-  , show expected
-  , " column(s), but found : "
-  , show actual
-  ]
+  fromString $
+    mconcat
+      [ "Expected ",
+        show expected,
+        " column(s), but found : ",
+        show actual
+      ]
 
 instance FromCol a => FromRow (Identity a) where
   fromRow row = case V.length row of
@@ -303,10 +308,10 @@ instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f) => F
       return (a, b, c, d, e, f)
     c -> throwError $ colMismatch 6 c
 
-
-instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g)
-  => FromRow (a, b, c, d, e, f, g) where
-
+instance
+  (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g) =>
+  FromRow (a, b, c, d, e, f, g)
+  where
   fromRow row = case V.length row of
     7 -> do
       a <- fromCol $ row V.! 0
@@ -319,10 +324,10 @@ instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, From
       return (a, b, c, d, e, f, g)
     c -> throwError $ colMismatch 7 c
 
-
-instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h)
-  => FromRow (a, b, c, d, e, f, g, h) where
-
+instance
+  (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h) =>
+  FromRow (a, b, c, d, e, f, g, h)
+  where
   fromRow row = case V.length row of
     8 -> do
       a <- fromCol $ row V.! 0
@@ -336,9 +341,10 @@ instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, From
       return (a, b, c, d, e, f, g, h)
     c -> throwError $ colMismatch 8 c
 
-instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h, FromCol i)
-  => FromRow (a, b, c, d, e, f, g, h, i) where
-
+instance
+  (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h, FromCol i) =>
+  FromRow (a, b, c, d, e, f, g, h, i)
+  where
   fromRow row = case V.length row of
     9 -> do
       a <- fromCol $ row V.! 0
@@ -353,9 +359,10 @@ instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, From
       return (a, b, c, d, e, f, g, h, i)
     c -> throwError $ colMismatch 9 c
 
-instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h, FromCol i, FromCol j)
-  => FromRow (a, b, c, d, e, f, g, h, i, j) where
-
+instance
+  (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h, FromCol i, FromCol j) =>
+  FromRow (a, b, c, d, e, f, g, h, i, j)
+  where
   fromRow row = case V.length row of
     10 -> do
       a <- fromCol $ row V.! 0
@@ -371,9 +378,10 @@ instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, From
       return (a, b, c, d, e, f, g, h, i, j)
     c -> throwError $ colMismatch 10 c
 
-instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h, FromCol i, FromCol j, FromCol k)
-  => FromRow (a, b, c, d, e, f, g, h, i, j, k) where
-
+instance
+  (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h, FromCol i, FromCol j, FromCol k) =>
+  FromRow (a, b, c, d, e, f, g, h, i, j, k)
+  where
   fromRow row = case V.length row of
     11 -> do
       a <- fromCol $ row V.! 0
@@ -390,9 +398,10 @@ instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, From
       return (a, b, c, d, e, f, g, h, i, j, k)
     c -> throwError $ colMismatch 11 c
 
-instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h, FromCol i, FromCol j, FromCol k, FromCol l)
-  => FromRow (a, b, c, d, e, f, g, h, i, j, k, l) where
-
+instance
+  (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, FromCol g, FromCol h, FromCol i, FromCol j, FromCol k, FromCol l) =>
+  FromRow (a, b, c, d, e, f, g, h, i, j, k, l)
+  where
   fromRow row = case V.length row of
     12 -> do
       a <- fromCol $ row V.! 0
@@ -410,10 +419,10 @@ instance (FromCol a, FromCol b, FromCol c, FromCol d, FromCol e, FromCol f, From
       return (a, b, c, d, e, f, g, h, i, j, k, l)
     c -> throwError $ colMismatch 12 c
 
-
 class FromRow a where
-  fromRow :: ResultRow
-          -> Either T.Text a
+  fromRow ::
+    ResultRow ->
+    Either T.Text a
 
 class ToPrepArgs a where
   toPrepArgs :: a -> [PrepArg]
@@ -431,7 +440,7 @@ toPrepValHelper :: PQ.Oid -> (a -> PE.Encoding) -> a -> PrepArg
 toPrepValHelper o e a = (o, Just (PE.encodingBytes $ e a, PQ.Binary))
 
 instance (J.ToJSON a) => ToPrepArg (AltJ a) where
-  toPrepVal (AltJ a)  = toPrepValHelper PTI.json PE.bytea_lazy $ J.encode a
+  toPrepVal (AltJ a) = toPrepValHelper PTI.json PE.bytea_lazy $ J.encode a
 
 instance ToPrepArg Word64 where
   toPrepVal = toPrepValHelper PTI.int8 PE.int8_word64
@@ -484,8 +493,9 @@ instance ToPrepArg Day where
 instance ToPrepArg UUID.UUID where
   toPrepVal = toPrepValHelper PTI.uuid PE.uuid
 
-newtype JSON  = JSON J.Value deriving (Eq, Show)
-newtype JSONB =  JSONB J.Value deriving (Eq, Show)
+newtype JSON = JSON J.Value deriving (Eq, Show)
+
+newtype JSONB = JSONB J.Value deriving (Eq, Show)
 
 instance ToPrepArg JSON where
   toPrepVal (JSON j) = toPrepValHelper PTI.json PE.json_ast j
@@ -496,107 +506,110 @@ instance ToPrepArg JSONB where
 instance (ToPrepArg a) => ToPrepArg (Maybe a) where
   toPrepVal (Just a) = toPrepVal a
   -- FIX ME, the oid here should be particular to the type
-  toPrepVal Nothing  = (PTI.auto, Nothing)
+  toPrepVal Nothing = (PTI.auto, Nothing)
 
 instance (ToPrepArg a) => ToPrepArgs [a] where
   toPrepArgs = map toPrepVal
 
 instance (ToPrepArg a) => ToPrepArgs (Identity a) where
   toPrepArgs (Identity a) =
-    [ toPrepVal a ]
+    [toPrepVal a]
 
 instance (ToPrepArg a, ToPrepArg b) => ToPrepArgs (a, b) where
   toPrepArgs (a, b) =
-    [ toPrepVal a
-    , toPrepVal b
+    [ toPrepVal a,
+      toPrepVal b
     ]
 
 instance (ToPrepArg a, ToPrepArg b, ToPrepArg c) => ToPrepArgs (a, b, c) where
   toPrepArgs (a, b, c) =
-    [ toPrepVal a
-    , toPrepVal b
-    , toPrepVal c
+    [ toPrepVal a,
+      toPrepVal b,
+      toPrepVal c
     ]
 
 instance (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d) => ToPrepArgs (a, b, c, d) where
   toPrepArgs (a, b, c, d) =
-    [ toPrepVal a
-    , toPrepVal b
-    , toPrepVal c
-    , toPrepVal d
+    [ toPrepVal a,
+      toPrepVal b,
+      toPrepVal c,
+      toPrepVal d
     ]
 
 instance (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e) => ToPrepArgs (a, b, c, d, e) where
   toPrepArgs (a, b, c, d, e) =
-    [ toPrepVal a
-    , toPrepVal b
-    , toPrepVal c
-    , toPrepVal d
-    , toPrepVal e
+    [ toPrepVal a,
+      toPrepVal b,
+      toPrepVal c,
+      toPrepVal d,
+      toPrepVal e
     ]
 
 instance (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e, ToPrepArg f) => ToPrepArgs (a, b, c, d, e, f) where
   toPrepArgs (a, b, c, d, e, f) =
-    [ toPrepVal a
-    , toPrepVal b
-    , toPrepVal c
-    , toPrepVal d
-    , toPrepVal e
-    , toPrepVal f
+    [ toPrepVal a,
+      toPrepVal b,
+      toPrepVal c,
+      toPrepVal d,
+      toPrepVal e,
+      toPrepVal f
     ]
 
 instance (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e, ToPrepArg f, ToPrepArg g) => ToPrepArgs (a, b, c, d, e, f, g) where
   toPrepArgs (a, b, c, d, e, f, g) =
-    [ toPrepVal a
-    , toPrepVal b
-    , toPrepVal c
-    , toPrepVal d
-    , toPrepVal e
-    , toPrepVal f
-    , toPrepVal g
+    [ toPrepVal a,
+      toPrepVal b,
+      toPrepVal c,
+      toPrepVal d,
+      toPrepVal e,
+      toPrepVal f,
+      toPrepVal g
     ]
 
-instance (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e, ToPrepArg f, ToPrepArg g, ToPrepArg h)
-  => ToPrepArgs (a, b, c, d, e, f, g, h) where
-
+instance
+  (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e, ToPrepArg f, ToPrepArg g, ToPrepArg h) =>
+  ToPrepArgs (a, b, c, d, e, f, g, h)
+  where
   toPrepArgs (a, b, c, d, e, f, g, h) =
-    [ toPrepVal a
-    , toPrepVal b
-    , toPrepVal c
-    , toPrepVal d
-    , toPrepVal e
-    , toPrepVal f
-    , toPrepVal g
-    , toPrepVal h
+    [ toPrepVal a,
+      toPrepVal b,
+      toPrepVal c,
+      toPrepVal d,
+      toPrepVal e,
+      toPrepVal f,
+      toPrepVal g,
+      toPrepVal h
     ]
 
-instance (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e, ToPrepArg f, ToPrepArg g, ToPrepArg h, ToPrepArg i)
-  => ToPrepArgs (a, b, c, d, e, f, g, h, i) where
-
+instance
+  (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e, ToPrepArg f, ToPrepArg g, ToPrepArg h, ToPrepArg i) =>
+  ToPrepArgs (a, b, c, d, e, f, g, h, i)
+  where
   toPrepArgs (a, b, c, d, e, f, g, h, i) =
-    [ toPrepVal a
-    , toPrepVal b
-    , toPrepVal c
-    , toPrepVal d
-    , toPrepVal e
-    , toPrepVal f
-    , toPrepVal g
-    , toPrepVal h
-    , toPrepVal i
+    [ toPrepVal a,
+      toPrepVal b,
+      toPrepVal c,
+      toPrepVal d,
+      toPrepVal e,
+      toPrepVal f,
+      toPrepVal g,
+      toPrepVal h,
+      toPrepVal i
     ]
 
-instance (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e, ToPrepArg f, ToPrepArg g, ToPrepArg h, ToPrepArg i, ToPrepArg j)
-  => ToPrepArgs (a, b, c, d, e, f, g, h, i, j) where
-
+instance
+  (ToPrepArg a, ToPrepArg b, ToPrepArg c, ToPrepArg d, ToPrepArg e, ToPrepArg f, ToPrepArg g, ToPrepArg h, ToPrepArg i, ToPrepArg j) =>
+  ToPrepArgs (a, b, c, d, e, f, g, h, i, j)
+  where
   toPrepArgs (a, b, c, d, e, f, g, h, i, j) =
-    [ toPrepVal a
-    , toPrepVal b
-    , toPrepVal c
-    , toPrepVal d
-    , toPrepVal e
-    , toPrepVal f
-    , toPrepVal g
-    , toPrepVal h
-    , toPrepVal i
-    , toPrepVal j
+    [ toPrepVal a,
+      toPrepVal b,
+      toPrepVal c,
+      toPrepVal d,
+      toPrepVal e,
+      toPrepVal f,
+      toPrepVal g,
+      toPrepVal h,
+      toPrepVal i,
+      toPrepVal j
     ]
--- src/Database/PG/Query/Listen.hs
+++ src/Database/PG/Query/Listen.hs
@@ -1,58 +1,60 @@
-{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- Reference:- https://github.com/hasura/skor/blob/master/src/skor.c
 
 module Database.PG.Query.Listen
-  ( PGChannel(..)
-  , NotifyHandler
-  , PGNotifyEvent(..)
-  , listen
+  ( PGChannel (..),
+    NotifyHandler,
+    PGNotifyEvent (..),
+    listen,
   )
 where
 
-import           Database.PG.Query.Connection
-import           Database.PG.Query.Pool
-import           Database.PG.Query.Transaction
+import Control.Exception (displayException, try)
+import Control.Monad.Except
+import Control.Monad.Trans.Control
+import Data.String
+import Data.Text qualified as T
+import Database.PG.Query.Connection
+import Database.PG.Query.Pool
+import Database.PG.Query.Transaction
+import Database.PostgreSQL.LibPQ qualified as PQ
+import GHC.Conc.IO (threadWaitRead)
 
-import           Control.Exception             (displayException, try)
-import           Control.Monad.Except
-import           Control.Monad.Trans.Control
-import           Data.String
-import           GHC.Conc.IO                   (threadWaitRead)
-
-import qualified Data.Text                     as T
-import qualified Database.PostgreSQL.LibPQ     as PQ
-
-newtype PGChannel
-  = PGChannel {getChannelTxt :: T.Text}
-  deriving(Show, Eq, IsString)
+newtype PGChannel = PGChannel {getChannelTxt :: T.Text}
+  deriving (Show, Eq, IsString)
 
 data PGNotifyEvent
   = PNEOnStart
   | PNEPQNotify !PQ.Notify
-  deriving Show
+  deriving (Show)
 
 type NotifyHandler = PGNotifyEvent -> IO ()
 
 -- | listen on given channel
-listen
-  :: ( FromPGConnErr e
-     , FromPGTxErr e
-     , MonadError e m
-     , MonadIO m
-     , MonadBaseControl IO m
-     )
-  => PGPool -> PGChannel -> NotifyHandler -> m ()
+listen ::
+  ( FromPGConnErr e,
+    FromPGTxErr e,
+    MonadError e m,
+    MonadIO m,
+    MonadBaseControl IO m
+  ) =>
+  PGPool ->
+  PGChannel ->
+  NotifyHandler ->
+  m ()
 listen pool channel handler = catchConnErr $
   withExpiringPGconn pool $ \pgConn -> do
     let conn = pgPQConn pgConn
 
     -- Issue listen command
-    eRes <- liftIO $ runExceptT $
-            execMulti pgConn (mkTemplate listenCmd) $ const $ return ()
+    eRes <-
+      liftIO $
+        runExceptT $
+          execMulti pgConn (mkTemplate listenCmd) $ const $ return ()
     either throwTxErr return eRes
     -- Emit onStart event
     liftIO $ handler PNEOnStart
@@ -94,5 +96,5 @@ waitForReadReadiness conn = do
     ioErrorToPGConnErr = PGConnErr . T.pack . displayException
 
 onJust :: Monad m => Maybe a -> (a -> m ()) -> m ()
-onJust Nothing _    = return ()
+onJust Nothing _ = return ()
 onJust (Just v) act = act v
