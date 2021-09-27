b2cfd35e625e6198e4d8b0e96752a4f8b588a319
Robert Vollmert <rob@vllmrt.net>
2021-09-27 11:05:17 +0200

    Revert "Shrink libpq buffers to 1MB before returning connection to pool"
    This reverts commit fbb635023228bba8a4abbaf1052fb03248eaf566.
    
    It's been unused for long enough, and it's all there in the history.

--- cbits/libpq-bindings.h
+++
@@ -1,6 +0,0 @@
-#include <internal/postgres_fe.h>
-#include <stdio.h>
-#include "libpq-fe.h"
-
-/* Shrink input and output buffers. See libpq.c */
-extern void PQclampInOutBufferSpace(PGconn *conn);
