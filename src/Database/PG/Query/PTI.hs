4a8f9a97830054dd7116a97af25355628c5115d7
Tom Harding <tomjharding@live.co.uk>
2022-06-23 15:19:22 +0100

    Add `arrayOf` for Oids (#70)
    * Add the `arrayOf` function for Oids
    
    There's now a function that maps Oids to their array counterparts based
    on the postgres definition[1] file. I've also swapped out the constants
    in favour of pattern synonyms to make that function 100x easier to
    read and maintain.
    
    [1]: https://github.com/postgres/postgres/blob/master/src/include/catalog/pg_type.dat
    
    * Stick to the types already there.
    
    * Capitalisation changes
    
    * More patterns please
    
    * Ormolu
    
    * Remove prefixes

--- src/Database/PG/Query/PTI.hs
+++ src/Database/PG/Query/PTI.hs
@@ -1,161 +1,484 @@
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PatternSynonyms #-}
 
+-- |
+-- Description: Oid constants.
+--
+-- All these are taken from this declaration file in the postgres repo:
+-- https://github.com/postgres/postgres/blob/master/src/include/catalog/pg_type.dat
 module Database.PG.Query.PTI where
 
--------------------------------------------------------------------------------
+import Database.PostgreSQL.LibPQ (Oid (..))
 
-import Data.Word (Word32)
-import Database.PostgreSQL.LibPQ qualified as PQ
-import Prelude
-
--------------------------------------------------------------------------------
-
-mkOid :: Word32 -> PQ.Oid
-mkOid = PQ.Oid . fromIntegral
+-- | Convert an 'Oid' for a single type into the corresponding array 'Oid'. If
+-- the original Oid is 'Auto', then the result will also be 'Auto'. For types
+-- without a defined array parallel, the result will be 'Unknown'.
+arrayOf :: Oid -> Oid
+arrayOf = \case
+  Auto -> Auto
+  ACLItem -> ACLItemArray
+  Bit -> BitArray
+  Bool -> BoolArray
+  Box -> BoxArray
+  BPChar -> BPCharArray
+  Bytea -> ByteaArray
+  Char -> CharArray
+  CID -> CIDArray
+  CIDR -> CIDRArray
+  Circle -> CircleArray
+  CString -> CStringArray
+  Date -> DateArray
+  DateRange -> DateRangeArray
+  Float4 -> Float4Array
+  Float8 -> Float8Array
+  GTSVector -> GTSVectorArray
+  INet -> INetArray
+  Int2 -> Int2Array
+  Int2Vector -> Int2VectorArray
+  Int4 -> Int4Array
+  Int4Range -> Int4RangeArray
+  Int8 -> Int8Array
+  Int8Range -> Int8RangeArray
+  Interval -> IntervalArray
+  JSON -> JSONArray
+  JSONB -> JSONBArray
+  Line -> LineArray
+  LSeg -> LSegArray
+  MacAddr -> MacAddrArray
+  Money -> MoneyArray
+  Name -> NameArray
+  Numeric -> NumericArray
+  NumRange -> NumRangeArray
+  OID -> OIDArray
+  OIDVector -> OIDVectorArray
+  Path -> PathArray
+  Point -> PointArray
+  Polygon -> PolygonArray
+  RefCursor -> RefCursorArray
+  RegClass -> RegClassArray
+  RegConfig -> RegConfigArray
+  RegDictionary -> RegDictionaryArray
+  RegOper -> RegOperArray
+  RegOperator -> RegOperatorArray
+  RegProc -> RegProcArray
+  RegProcedure -> RegProcedureArray
+  RegType -> RegTypeArray
+  Text -> TextArray
+  TID -> TIDArray
+  Time -> TimeArray
+  Timestamp -> TimestampArray
+  TimestampTZ -> TimestampTZArray
+  TimeTZ -> TimeTZArray
+  TSQuery -> TSQueryArray
+  TSRange -> TSRangeArray
+  TSTZRange -> TSTZRangeArray
+  TSVector -> TSVectorArray
+  UUID -> UUIDArray
+  Varbit -> VarbitArray
+  Varchar -> VarcharArray
+  XID -> XIDArray
+  XML -> XMLArray
+  _ -> Unknown
 
 -- * Constants
 
--------------------------
-auto = mkOid 0
+--
+-- We use pattern synonyms for the Oids so that we can pattern-match on them.
 
-abstime = mkOid 702
+pattern Auto :: Oid
+pattern Auto = Oid 0
 
-aclitem = mkOid 1033
+pattern AbsTime :: Oid
+pattern AbsTime = Oid 702
 
-bit = mkOid 1560
+pattern ACLItem :: Oid
+pattern ACLItem = Oid 1033
 
-bool = mkOid 16
+pattern Bit :: Oid
+pattern Bit = Oid 1560
 
-box = mkOid 603
+pattern Bool :: Oid
+pattern Bool = Oid 16
 
-bpchar = mkOid 1042
+pattern Box :: Oid
+pattern Box = Oid 603
 
-bytea = mkOid 17
+pattern BPChar :: Oid
+pattern BPChar = Oid 1042
 
-char = mkOid 18
+pattern Bytea :: Oid
+pattern Bytea = Oid 17
 
-cid = mkOid 29
+pattern Char :: Oid
+pattern Char = Oid 18
 
-cidr = mkOid 650
+pattern CID :: Oid
+pattern CID = Oid 29
 
-circle = mkOid 718
+pattern CIDR :: Oid
+pattern CIDR = Oid 650
 
-cstring = mkOid 2275
+pattern Circle :: Oid
+pattern Circle = Oid 718
 
-date = mkOid 1082
+pattern CString :: Oid
+pattern CString = Oid 2275
 
-daterange = mkOid 3912
+pattern Date :: Oid
+pattern Date = Oid 1082
 
-float4 = mkOid 700
+pattern DateRange :: Oid
+pattern DateRange = Oid 3912
 
-float8 = mkOid 701
+pattern Float4 :: Oid
+pattern Float4 = Oid 700
 
-gtsvector = mkOid 3642
+pattern Float8 :: Oid
+pattern Float8 = Oid 701
 
-inet = mkOid 869
+pattern GTSVector :: Oid
+pattern GTSVector = Oid 3642
 
-int2 = mkOid 21
+pattern INet :: Oid
+pattern INet = Oid 869
 
-int2vector = mkOid 22
+pattern Int2 :: Oid
+pattern Int2 = Oid 21
 
-int4 = mkOid 23
+pattern Int2Vector :: Oid
+pattern Int2Vector = Oid 22
 
-int4range = mkOid 3904
+pattern Int4 :: Oid
+pattern Int4 = Oid 23
 
-int8 = mkOid 20
+pattern Int4Range :: Oid
+pattern Int4Range = Oid 3904
 
-int8range = mkOid 3926
+pattern Int8 :: Oid
+pattern Int8 = Oid 20
 
-interval = mkOid 1186
+pattern Int8Range :: Oid
+pattern Int8Range = Oid 3926
 
-json = mkOid 114
+pattern Interval :: Oid
+pattern Interval = Oid 1186
 
-jsonb = mkOid 3802
+pattern JSON :: Oid
+pattern JSON = Oid 114
 
-line = mkOid 628
+pattern JSONB :: Oid
+pattern JSONB = Oid 3802
 
-lseg = mkOid 601
+pattern Line :: Oid
+pattern Line = Oid 628
 
-macaddr = mkOid 829
+pattern LSeg :: Oid
+pattern LSeg = Oid 601
 
-money = mkOid 790
+pattern MacAddr :: Oid
+pattern MacAddr = Oid 829
 
-name = mkOid 19
+pattern Money :: Oid
+pattern Money = Oid 790
 
-numeric = mkOid 1700
+pattern Name :: Oid
+pattern Name = Oid 19
 
-numrange = mkOid 3906
+pattern Numeric :: Oid
+pattern Numeric = Oid 1700
 
-oid = mkOid 26
+pattern NumRange :: Oid
+pattern NumRange = Oid 3906
 
-oidvector = mkOid 30
+pattern OID :: Oid
+pattern OID = Oid 26
 
-path = mkOid 602
+pattern OIDVector :: Oid
+pattern OIDVector = Oid 30
 
-point = mkOid 600
+pattern Path :: Oid
+pattern Path = Oid 602
 
-polygon = mkOid 604
+pattern Point :: Oid
+pattern Point = Oid 600
 
-record = mkOid 2249
+pattern Polygon :: Oid
+pattern Polygon = Oid 604
 
-refcursor = mkOid 1790
+pattern Record :: Oid
+pattern Record = Oid 2249
 
-regclass = mkOid 2205
+pattern RefCursor :: Oid
+pattern RefCursor = Oid 1790
 
-regconfig = mkOid 3734
+pattern RegClass :: Oid
+pattern RegClass = Oid 2205
 
-regdictionary = mkOid 3769
+pattern RegConfig :: Oid
+pattern RegConfig = Oid 3734
 
-regoper = mkOid 2203
+pattern RegDictionary :: Oid
+pattern RegDictionary = Oid 3769
 
-regoperator = mkOid 2204
+pattern RegOper :: Oid
+pattern RegOper = Oid 2203
 
-regproc = mkOid 24
+pattern RegOperator :: Oid
+pattern RegOperator = Oid 2204
 
-regprocedure = mkOid 2202
+pattern RegProc :: Oid
+pattern RegProc = Oid 24
 
-regtype = mkOid 2206
+pattern RegProcedure :: Oid
+pattern RegProcedure = Oid 2202
 
-reltime = mkOid 703
+pattern RegType :: Oid
+pattern RegType = Oid 2206
 
-text = mkOid 25
+pattern RelTime :: Oid
+pattern RelTime = Oid 703
 
-tid = mkOid 27
+pattern Text :: Oid
+pattern Text = Oid 25
 
-time = mkOid 1083
+pattern TID :: Oid
+pattern TID = Oid 27
 
-timestamp = mkOid 1114
+pattern Time :: Oid
+pattern Time = Oid 1083
 
-timestamptz = mkOid 1184
+pattern Timestamp :: Oid
+pattern Timestamp = Oid 1114
 
-timetz = mkOid 1266
+pattern TimestampTZ :: Oid
+pattern TimestampTZ = Oid 1184
 
-tinterval = mkOid 704
+pattern TimeTZ :: Oid
+pattern TimeTZ = Oid 1266
 
-tsquery = mkOid 3615
+pattern TInterval :: Oid
+pattern TInterval = Oid 704
 
-tsrange = mkOid 3908
+pattern TSQuery :: Oid
+pattern TSQuery = Oid 3615
 
-tstzrange = mkOid 3910
+pattern TSRange :: Oid
+pattern TSRange = Oid 3908
 
-tsvector = mkOid 3614
+pattern TSTZRange :: Oid
+pattern TSTZRange = Oid 3910
 
-txid_snapshot = mkOid 2970
+pattern TSVector :: Oid
+pattern TSVector = Oid 3614
 
-unknown = mkOid 705
+pattern TXIDSnapshot :: Oid
+pattern TXIDSnapshot = Oid 2970
 
-uuid = mkOid 2950
+pattern Unknown :: Oid
+pattern Unknown = Oid 705
 
-varbit = mkOid 1562
+pattern UUID :: Oid
+pattern UUID = Oid 2950
 
-varchar = mkOid 1043
+pattern Varbit :: Oid
+pattern Varbit = Oid 1562
 
-void = mkOid 2278
+pattern Varchar :: Oid
+pattern Varchar = Oid 1043
 
-xid = mkOid 28
+pattern Void :: Oid
+pattern Void = Oid 2278
 
-xml = mkOid 142
+pattern XID :: Oid
+pattern XID = Oid 28
+
+pattern XML :: Oid
+pattern XML = Oid 142
 
 -- Array Types
 
-text_arr = mkOid 1009
+pattern ACLItemArray :: Oid
+pattern ACLItemArray = Oid 1034
+
+pattern BitArray :: Oid
+pattern BitArray = Oid 1561
+
+pattern BoolArray :: Oid
+pattern BoolArray = Oid 1000
+
+pattern BoxArray :: Oid
+pattern BoxArray = Oid 1020
+
+pattern BPCharArray :: Oid
+pattern BPCharArray = Oid 1014
+
+pattern ByteaArray :: Oid
+pattern ByteaArray = Oid 1001
+
+pattern CharArray :: Oid
+pattern CharArray = Oid 1002
+
+pattern CIDArray :: Oid
+pattern CIDArray = Oid 1012
+
+pattern CIDRArray :: Oid
+pattern CIDRArray = Oid 651
+
+pattern CircleArray :: Oid
+pattern CircleArray = Oid 719
+
+pattern CStringArray :: Oid
+pattern CStringArray = Oid 1263
+
+pattern DateArray :: Oid
+pattern DateArray = Oid 1182
+
+pattern DateRangeArray :: Oid
+pattern DateRangeArray = Oid 3913
+
+pattern Float4Array :: Oid
+pattern Float4Array = Oid 1021
+
+pattern Float8Array :: Oid
+pattern Float8Array = Oid 1022
+
+pattern GTSVectorArray :: Oid
+pattern GTSVectorArray = Oid 3644
+
+pattern INetArray :: Oid
+pattern INetArray = Oid 1041
+
+pattern Int2Array :: Oid
+pattern Int2Array = Oid 1005
+
+pattern Int2VectorArray :: Oid
+pattern Int2VectorArray = Oid 1006
+
+pattern Int4Array :: Oid
+pattern Int4Array = Oid 1007
+
+pattern Int4RangeArray :: Oid
+pattern Int4RangeArray = Oid 3905
+
+pattern Int8Array :: Oid
+pattern Int8Array = Oid 1016
+
+pattern Int8RangeArray :: Oid
+pattern Int8RangeArray = Oid 3927
+
+pattern IntervalArray :: Oid
+pattern IntervalArray = Oid 1187
+
+pattern JSONArray :: Oid
+pattern JSONArray = Oid 199
+
+pattern JSONBArray :: Oid
+pattern JSONBArray = Oid 3807
+
+pattern LineArray :: Oid
+pattern LineArray = Oid 629
+
+pattern LSegArray :: Oid
+pattern LSegArray = Oid 1018
+
+pattern MacAddrArray :: Oid
+pattern MacAddrArray = Oid 1040
+
+pattern MoneyArray :: Oid
+pattern MoneyArray = Oid 791
+
+pattern NameArray :: Oid
+pattern NameArray = Oid 1003
+
+pattern NumericArray :: Oid
+pattern NumericArray = Oid 1231
+
+pattern NumRangeArray :: Oid
+pattern NumRangeArray = Oid 3907
+
+pattern OIDArray :: Oid
+pattern OIDArray = Oid 1028
+
+pattern OIDVectorArray :: Oid
+pattern OIDVectorArray = Oid 1013
+
+pattern PathArray :: Oid
+pattern PathArray = Oid 1019
+
+pattern PointArray :: Oid
+pattern PointArray = Oid 1017
+
+pattern PolygonArray :: Oid
+pattern PolygonArray = Oid 1027
+
+pattern RefCursorArray :: Oid
+pattern RefCursorArray = Oid 2201
+
+pattern RegClassArray :: Oid
+pattern RegClassArray = Oid 2210
+
+pattern RegConfigArray :: Oid
+pattern RegConfigArray = Oid 3735
+
+pattern RegDictionaryArray :: Oid
+pattern RegDictionaryArray = Oid 3770
+
+pattern RegOperArray :: Oid
+pattern RegOperArray = Oid 2208
+
+pattern RegOperatorArray :: Oid
+pattern RegOperatorArray = Oid 2209
+
+pattern RegProcArray :: Oid
+pattern RegProcArray = Oid 1008
+
+pattern RegProcedureArray :: Oid
+pattern RegProcedureArray = Oid 2207
+
+pattern RegTypeArray :: Oid
+pattern RegTypeArray = Oid 2211
+
+pattern TextArray :: Oid
+pattern TextArray = Oid 1009
+
+pattern TIDArray :: Oid
+pattern TIDArray = Oid 1010
+
+pattern TimeArray :: Oid
+pattern TimeArray = Oid 1183
+
+pattern TimestampArray :: Oid
+pattern TimestampArray = Oid 1115
+
+pattern TimestampTZArray :: Oid
+pattern TimestampTZArray = Oid 1185
+
+pattern TimeTZArray :: Oid
+pattern TimeTZArray = Oid 1270
+
+pattern TSQueryArray :: Oid
+pattern TSQueryArray = Oid 3645
+
+pattern TSRangeArray :: Oid
+pattern TSRangeArray = Oid 3909
+
+pattern TSTZRangeArray :: Oid
+pattern TSTZRangeArray = Oid 3911
+
+pattern TSVectorArray :: Oid
+pattern TSVectorArray = Oid 3643
+
+pattern UUIDArray :: Oid
+pattern UUIDArray = Oid 2951
+
+pattern VarbitArray :: Oid
+pattern VarbitArray = Oid 1563
+
+pattern VarcharArray :: Oid
+pattern VarcharArray = Oid 1015
+
+pattern XIDArray :: Oid
+pattern XIDArray = Oid 1011
+
+pattern XMLArray :: Oid
+pattern XMLArray = Oid 143
